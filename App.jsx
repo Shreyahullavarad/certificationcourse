d214625e6e2437162375e51fe37af1346bfe2493
Shreya Hullavarad <shreyahullavarad@gmail.com>
2025-09-21 22:56:50 +0530

    Delete App.jsx

--- App.jsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-    return (
-        <form>
-            <input type="text" />
-            <button type="submit">submit</button>
-        </form>
-    );
-    //in above we are using (),as it has many tags.
-}
